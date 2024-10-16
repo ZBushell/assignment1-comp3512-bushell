77a31063b14edd2bf6f7e788a6084bc79bda3c8e
Zach-Bushell <zachbushell11@gmail.com>
2024-10-16 12:15:31 -0600

    second api file

---
+++ api/drivers.php
@@ -0,0 +1,31 @@
+<?php
+    //been having issues with pathing on my php install. this is here to fix that.
+    chdir("../");
+    //sql queries and dababase object
+    require_once "./includes/config.inc.php";
+    require "./includes/queries.inc.php";
+
+
+    //make sure php knows what its outputting
+    header("Content-Type: application/json; charset=UTF-8");
+
+    //variables 
+    $db = new F1Database("./data/f1.db");
+    $queryResults;
+    
+    // check if we're returning everything or just one circuit
+    if(isset($_GET['ref']) && $_GET['ref'] != null){
+
+        $queryResults = $db->preparedQuery($specificDriverIn2022, $_GET['ref']);
+    }
+    else {
+        
+        $queryResults = $db->pdoQuery($driversIn2022);
+    } 
+
+    //close db session
+    $db->dropConnection();
+    
+    //encode and print
+    print (json_encode($queryResults));
+?>
--- includes/queries.inc.php
+++ includes/queries.inc.php
@@ -1,72 +1,72 @@
 <?php
 // Drivers in 2022
-$driversIn2022Query = "SELECT DISTINCT d.* FROM drivers d 
+$driversIn2022 = "SELECT DISTINCT d.* FROM drivers d 
                         LEFT JOIN qualifying q ON d.driverId = q.driverId 
                         LEFT JOIN races r ON q.raceId = r.raceId 
                         WHERE r.year IN(2022);";
 
 // Specific driver in 2022
-$specificDriverIn2022Query = "SELECT DISTINCT d.* FROM drivers d 
+$specificDriverIn2022 = "SELECT DISTINCT d.* FROM drivers d 
                                 LEFT JOIN qualifying q ON d.driverId = q.driverId 
                                 LEFT JOIN races r ON q.raceId = r.raceId 
-                                WHERE r.year IN(2022) AND d.driverRef IN('hamilton');";
+                                WHERE r.year IN(2022) AND d.driverRef IN( ? );";
 
 // Constructors in 2022
-$constructorsIn2022Query = "SELECT DISTINCT c.* FROM constructors c 
+$constructorsIn2022 = "SELECT DISTINCT c.* FROM constructors c 
                              LEFT JOIN qualifying q ON c.constructorId = q.constructorId 
                              LEFT JOIN races r ON q.raceId = r.raceId 
                              WHERE r.year IN(2022);";
 
 // Specific constructor from 2022
-$specificConstructorIn2022Query = "SELECT DISTINCT c.* FROM constructors c 
+$specificConstructorIn2022 = "SELECT DISTINCT c.* FROM constructors c 
                                      LEFT JOIN qualifying q ON c.constructorId = q.constructorId 
                                      LEFT JOIN races r ON q.raceId = r.raceId 
-                                     WHERE r.year IN(2022) AND c.constructorRef IN('red_bull');";
+                                     WHERE r.year IN(2022) AND c.constructorRef IN( ? );";
 
 // Circuits in 2022
-$circuitsIn2022Query = "SELECT DISTINCT c.* FROM circuits c 
+$circuitsIn2022 = "SELECT DISTINCT c.* FROM circuits c 
                          LEFT JOIN races r ON c.circuitID = r.circuitID 
                          WHERE r.year IN(2022) 
                          ORDER BY r.date DESC;";
 
 // Only one circuit
-$singleCircuitQuery = "SELECT DISTINCT c.* FROM circuits c 
+$singleCircuit = "SELECT DISTINCT c.* FROM circuits c 
                        LEFT JOIN races r ON c.circuitID = r.circuitID 
                        WHERE r.year IN(2022) AND c.circuitRef IN( ? ) 
                        ORDER BY r.date DESC;";
 
 // Driver with a specific race ID
-$driverWithSpecificRaceIdQuery = "SELECT DISTINCT d.* FROM drivers d 
+$driverWithSpecificRaceId = "SELECT DISTINCT d.* FROM drivers d 
                                    LEFT JOIN qualifying q ON d.driverId = q.driverId 
                                    LEFT JOIN races r ON q.raceId = r.raceId 
-                                   WHERE r.year IN(2022) AND r.raceId IN (1106);";
+                                   WHERE r.year IN(2022) AND r.raceId IN ( ? );";
 
 // Races in 2022
-$racesIn2022Query = "SELECT * FROM races r 
+$racesIn2022 = "SELECT * FROM races r 
                      WHERE r.year IN(2022) 
                      ORDER BY round;";
 
 // Specific race
-$specificRaceQuery = "SELECT r.name, c.name, c.location, c.country 
+$specificRace = "SELECT r.name, c.name, c.location, c.country 
                       FROM races r 
                       LEFT JOIN results re ON r.raceId = re.raceId 
                       LEFT JOIN circuits c ON r.circuitID = c.circuitId 
                       WHERE r.raceId IN (1106);";
 
 // Results for a specific race
-$resultsForSpecificRaceQuery = "SELECT d.driverRef, d.code, d.forename, d.surname, 
+$resultsForSpecificRace = "SELECT d.driverRef, d.code, d.forename, d.surname, 
                                  ra.name, ra.round, ra.year, ra.date, 
                                  c.name, c.constructorRef, c.nationality 
                                  FROM drivers d 
                                  LEFT JOIN results r ON d.driverID = r.driverID 
                                  LEFT JOIN constructors s ON s.constructorId = r.constructorId 
                                  LEFT JOIN races ra ON ra.raceId = r.raceId 
-                                 WHERE r.raceId IN (1106) 
+                                 WHERE r.raceId IN ( ? ) 
                                  ORDER BY r.grid ASC;";
 
 // Results for a given driver
-$resultsForGivenDriverQuery = "SELECT r.* FROM results r 
+$resultsForGivenDriver = "SELECT r.* FROM results r 
                                LEFT JOIN drivers d ON r.driverID = d.driverID 
-                               WHERE d.driverRef IN ('verstappen');";
+                               WHERE d.driverRef IN ( ? );";
 
 ?>
